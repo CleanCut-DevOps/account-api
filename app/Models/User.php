0cf65b047f9d382937f2b17ff7f6e49f4ce6e93f
Zachary Lim <zacharylim2004@gmail.com>
2022-12-13 22:42:05 +0800

    Created authentication controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,50 @@
 namespace App\Models;
 
 use App\Traits\UUID;
+use Database\Factories\UserFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property string $id
+ * @property string $username
+ * @property string $email
+ * @property string $contact
+ * @property string $password
+ * @property string|null $avatar
+ * @property int|null $deleted_at
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static UserFactory factory(...$parameters)
+ * @method static EloquentBuilder|User newModelQuery()
+ * @method static EloquentBuilder|User newQuery()
+ * @method static QueryBuilder|User onlyTrashed()
+ * @method static EloquentBuilder|User query()
+ * @method static EloquentBuilder|User whereAvatar($value)
+ * @method static EloquentBuilder|User whereContact($value)
+ * @method static EloquentBuilder|User whereCreatedAt($value)
+ * @method static EloquentBuilder|User whereDeletedAt($value)
+ * @method static EloquentBuilder|User whereEmail($value)
+ * @method static EloquentBuilder|User whereId($value)
+ * @method static EloquentBuilder|User wherePassword($value)
+ * @method static EloquentBuilder|User whereUpdatedAt($value)
+ * @method static EloquentBuilder|User whereUsername($value)
+ * @method static QueryBuilder|User withTrashed()
+ * @method static QueryBuilder|User withoutTrashed()
+ * @mixin Eloquent
+ */
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, SoftDeletes, Notifiable, UUID;

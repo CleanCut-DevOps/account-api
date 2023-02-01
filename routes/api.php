57d899286da2d680be8c064dcc6fe63af1855d62
Zachary <zacharylim2004@gmail.com>
2023-02-01 18:20:47 +0800

    Init project

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AccountController;
-use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,19 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->json([
-        "type" => "Not found",
-        "message" => "There's nothing here..",
-    ], 404);
+    return response()->json(["type" => "Not found", "message" => "There's nothing here.."], 404);
 });
-
-// Authentication
-Route::post('login', [AuthenticationController::class, 'login']);
-Route::post('register', [AuthenticationController::class, 'register']);
-Route::post('logout', [AuthenticationController::class, 'logout']);
-Route::post('reset', [AuthenticationController::class, 'resetPassword']);
-
-// User Account
-Route::get('account', [AccountController::class, 'read']);
-Route::put('account', [AccountController::class, 'update']);
-Route::delete('account', [AccountController::class, 'delete']);

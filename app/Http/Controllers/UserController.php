4a4460d27947c492998bc9d07fc9d9246ad24343
Zachary <zacharylim2004@gmail.com>
2023-02-01 20:08:01 +0800

    Updated request validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -17,10 +19,56 @@ class UserController extends Controller
      * Set middleware for all functions
      *
      * @return void
+     * @throws ValidationException
      */
     public function __construct()
     {
-        $this->middleware(ValidateJWT::class)->except(["login", "register"]);
+        $this->middleware(ValidateJWT::class)->except(['login', 'register']);
+
+        $this->validate('login', [
+            'email' => ['required', 'email', 'max:255'],
+            'remember' => ['required', 'boolean'],
+            'password' => ['required', 'string', 'min:8'],
+        ], [
+            'email.required' => 'We need to know your email address',
+            'email.email' => 'Your email address is invalid',
+            'email.max' => 'Your email address is too long',
+            'remember.required' => 'Remember me field is required',
+            'remember.boolean' => 'Remember me value is invalid',
+            'password.required' => 'We need to know your password',
+            'password.string' => 'Your password is invalid',
+            'password.min' => 'Your password is too short',
+        ]);
+
+        $this->validate('register', [
+            "name" => ["required", "string", "max:255"],
+            "phone" => ["required", "string", "max:255"],
+            "email" => ["required", "string", "email", "max:255", "unique:user"],
+            "password" => ["required", Password::min(8)->mixedCase()->numbers()->uncompromised()],
+        ], [
+            "name.required" => "We need to who you are",
+            "name.string" => "Your name is invalid",
+            "name.max" => "Your name is too long",
+            "email.required" => "We need to know your email address",
+            "email.string" => "Your email address is invalid",
+            "email.email" => "Your email address is invalid",
+            "email.max" => "Your email address is too long",
+            "email.unique" => "Your email address is already in use",
+            "phone.required" => "We need to know how to contact you",
+            "phone.string" => "Your phone number is invalid",
+            "phone.max" => "Your phone number is too long",
+            "password.required" => "We need to know your password"
+        ]);
+
+        $this->validate('reset', [
+            "newPassword" => ["required", "string", Password::min(8)->mixedCase()->numbers()->uncompromised()],
+            "oldPassword" => ["required", "string", Password::default()],
+        ], [
+            "newPassword.required" => "Please indicate your new password",
+            "newPassword.string" => "Your new password is invalid",
+            "oldPassword.required" => "Please indicate your old password",
+            "oldPassword.string" => "Your old password is invalid",
+        ]);
     }
 
     /**
@@ -32,18 +80,18 @@ class UserController extends Controller
      */
     public function login(Request $request): JsonResponse
     {
-        $emailCred = $request->only("email", "password");
+        $emailCred = $request->only('email', 'password');
 
-        if ($token = Auth::setTTL(request("remember") ? 60 * 24 * 7 : 60 * 24)->attempt($emailCred)) {
+        if ($token = Auth::setTTL(request('remember') ? 60 * 24 * 7 : 60 * 24)->attempt($emailCred)) {
             return response()->json([
-                "type" => "Successful request",
-                "message" => "Logged in successfully",
-                "token" => $token
+                'type' => 'Successful request',
+                'message' => 'Logged in successfully',
+                'token' => $token
             ]);
         } else {
             return response()->json([
-                "type" => "Unauthorized",
-                "message" => "Invalid credentials"
+                'type' => 'Unauthorized',
+                'message' => 'Invalid credentials'
             ], 401);
         }
     }
@@ -57,15 +105,20 @@ class UserController extends Controller
      */
     public function register(Request $request): JsonResponse
     {
-        $request["password"] = Hash::make(request("password"));
+        $request['password'] = Hash::make(request('password'));
 
-        $user = User::create($request->all());
+        $user = User::create($request->only([
+            'name',
+            'email',
+            'phone',
+            'password'
+        ]));
 
         event(new Registered($user));
 
         return response()->json([
-            "type" => "Successful request",
-            "message" => "User created successfully"
+            'type' => 'Successful request',
+            'message' => 'User created successfully'
         ], 201);
     }
 
@@ -79,36 +132,34 @@ class UserController extends Controller
         Auth::logout();
 
         return response()->json([
-            "type" => "Successful request",
-            "message" => "User logged out successfully"
+            'type' => 'Successful request',
+            'message' => 'User logged out successfully'
         ], 200);
     }
 
     /**
      * Reset the user's password.
      *
-     * @param Request $request
-     *
      * @return JsonResponse
      */
-    public function resetPassword(Request $request): JsonResponse
+    public function reset(): JsonResponse
     {
-        $userID = Auth::payload()->get("sub");
+        $userID = Auth::payload()->get('sub');
 
         $user = User::find($userID);
 
-        if (Hash::check(request("oldPassword"), $user->password)) {
-            $user->password = Hash::make(request("newPassword"));
+        if (Hash::check(request('oldPassword'), $user->password)) {
+            $user->password = Hash::make(request('newPassword'));
             $user->save();
 
             return response()->json([
-                "type" => "Successful request",
-                "message" => "Password changed successfully"
+                'type' => 'Successful request',
+                'message' => 'Password changed successfully'
             ], 200);
         } else {
             return response()->json([
-                "type" => "Invalid data",
-                "message" => "Request data is invalid"
+                'type' => 'Invalid data',
+                'message' => 'Request data is invalid'
             ], 401);
         }
     }
@@ -122,9 +173,9 @@ class UserController extends Controller
     public function show(User $user): JsonResponse
     {
         return response()->json([
-            "type" => "Successful request",
-            "message" => "User data retrieved successfully",
-            "user" => $user
+            'type' => 'Successful request',
+            'message' => 'User data retrieved successfully',
+            'user' => $user
         ]);
     }
 
@@ -140,9 +191,9 @@ class UserController extends Controller
         $user->update($request->only(['email', 'phone', 'full_name', 'preferred_contact']));
 
         return response()->json([
-            "type" => "Successful request",
-            "message" => "User data updated successfully",
-            "user" => $user
+            'type' => 'Successful request',
+            'message' => 'User data updated successfully',
+            'user' => $user
         ]);
     }
 
@@ -159,8 +210,8 @@ class UserController extends Controller
         $user->delete();
 
         return response()->json([
-            "type" => "Successful request",
-            "message" => "User data deleted successfully"
+            'type' => 'Successful request',
+            'message' => 'User data deleted successfully'
         ]);
     }
 }

45569db49c627cd31d60f1d0e2c8fa7a46bfdbf6
Zachary <zacharylim2004@gmail.com>
2023-02-02 15:51:04 +0800

    Create user routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\VerifyEmailToken;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return response()->json(["type" => "Not found", "message" => "There's nothing here.."], 404);
+// User account routes
+Route::prefix('user')->group(function () {
+
+    Route::get('', [UserController::class, 'show']);
+    Route::put('', [UserController::class, 'update']);
+    Route::delete('', [UserController::class, 'destroy']);
+    Route::post('login', [UserController::class, 'login']);
+    Route::post('logout', [UserController::class, 'logout']);
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('reset', [UserController::class, 'reset']);
+
 });
+
+// Default routes
+
+// Email verification route
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    if (!$request->user()->hasVerifiedEmail()) {
+        $request->user()->markEmailAsVerified();
+
+        event(new Verified($request->user()));
+    }
+
+    $url = config('app.url');
+
+    return redirect($url);
+})->middleware([VerifyEmailToken::class, 'signed'])->name('verification.verify');
+
+// Catch-all route
+Route::get('{any}', function () {
+    return response()->json([
+        "type" => "Not found",
+        "message" => "There's nothing here.."
+    ], 404);
+})->where('any', '.*');
